59d55d206b14a4108df4f2e531695d6f0bfb6144
saeed <saeed.alnuaimi1@du.ae>
2021-10-13 13:35:48 +0400

    Added task marking

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,32 +1,47 @@
 <template>
   <div class="home">
-    <v-list
-      flat
-      three-line
-    >
-        <v-list-item>
+    <v-list flat three-line class="pt-0">
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item 
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5' : task.done }"
+        >
           <template v-slot:default="{ active }">
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox :input-value="task.done"></v-checkbox>
             </v-list-item-action>
-
             <v-list-item-content>
-              <v-list-item-title>Notifications</v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through' : task.done}"
+              >{{ task.title }}</v-list-item-title>
               <v-list-item-subtitle></v-list-item-subtitle>
             </v-list-item-content>
           </template>
         </v-list-item>
-
+        <v-divider></v-divider>
+      </div>
     </v-list>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-
-    components: {
+export default {
+  name: "Home",
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: "Wake Up", done: false },
+        { id: 2, title: "Get Groceries", done: false },
+        { id: 3, title: "Breakfast", done: false },
+      ],
+    };
+  },
+  methods: {
+    doneTask(id){
+      let task = this.tasks.filter(task => task.id === id )[0]
+      task.done = !task.done 
       
-    },
+    }
   }
+};
 </script>
