3634429d55792b8310f267ae0a81ff1458e184cd
saeed <saeed.alnuaimi1@du.ae>
2021-10-14 11:03:20 +0400

    Added delete task dialog

---
+++ src/components/Todo/Dialogs/DialogDelete.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-dialog
+      :value="true"
+      persistent
+      max-width="290"
+    >
+      <template v-slot:activator="{ on, attrs }">
+      </template>
+      <v-card>
+        <v-card-title class="text-h5">
+          Delete Task?
+        </v-card-title>
+        <v-card-text>Are you sure you want to delete this task</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            @click="$emit('closeDialog')"
+          >
+            No
+          </v-btn>
+          <v-btn
+            color="red darken-1"
+            text
+            @click="$store.dispatch('deleteTask', task.id)"
+          >
+            Yes
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    props: ['task']
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -1,32 +1,49 @@
 <template>
-        <div>
-        <v-list-item
-          @click="$store.commit('doneTask', task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default="{ active }">
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn @click.stop="$store.dispatch('deleteTask', task.id)" icon>
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
+  <div>
+    <v-list-item
+      @click="$store.commit('doneTask', task.id)"
+      :class="{ 'blue lighten-5': task.done }"
+    >
+      <template v-slot:default="{ active }">
+        <v-list-item-action>
+          <v-checkbox :input-value="task.done"></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title
+            :class="{ 'text-decoration-line-through': task.done }"
+            >{{ task.title }}</v-list-item-title
+          >
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn @click.stop="dialogs.delete = true" icon>
+            <v-icon color="primary lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </template>
+    </v-list-item>
+    <v-divider></v-divider>
+    <dialog-delete
+    v-if="dialogs.delete"
+    :task="task"
+    @closeDialog="dialogs.delete = false"
+    />
+  </div>
 </template>
 
 <script>
+import DialogDelete from "./Dialogs/DialogDelete.vue";
+
 export default {
-    props: ['task']
-}
+  data() {
+    return {
+      dialogs: {
+        delete: false,
+      },
+    };
+  },
+  props: ["task"],
+  components: {
+    "dialog-delete": DialogDelete,
+  },
+};
 </script>
