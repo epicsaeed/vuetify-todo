d3700fc6b6d942665a1d23d1ab51de3a83fdafee
saeed <saeed.alnuaimi1@du.ae>
2021-10-14 09:22:08 +0400

    Create Task Child Component

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,32 +1,7 @@
 <template>
   <div class="home">
     <field-add-task />
-    <v-list v-if="$store.state.tasks.length" flat class="pt-0">
-      <div v-for="task in $store.state.tasks" :key="task.id">
-        <v-list-item
-          @click="doneTask(task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default="{ active }">
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn @click.stop="deleteTask(task.id)" icon>
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
+    <list-tasks v-if="$store.state.tasks.length"/>
     <div class="no-tasks" v-else>
       <v-icon size="100px" color="primary">
         mdi-check
@@ -37,19 +12,13 @@
 </template>
 
 <script>
-import FieldAddTask from "../components/Todo/FieldAddTask.vue";
+import FieldAddTask from '../components/Todo/FieldAddTask.vue';
+import ListTasks from '../components/Todo/ListTasks.vue';
 
 export default {
-  methods: {
-    doneTask(id) {
-      this.$store.commit("doneTask", id);
-    },
-    deleteTask(id) {
-      this.$store.commit("deleteTask", id);
-    },
-  },
   components: {
     "field-add-task": FieldAddTask,
+    "list-tasks": ListTasks
   },
 };
 </script>
