df5d2ca7a94f2a758f34c4914a625959cb4a94ff
Saeed Alnuaimi <saeed.alnuaimi1@du.ae>
2021-10-27 08:59:49 +0400

    Fixed snackbar

--- src/components/Snackbar.vue
+++ src/components/Snackbar.vue
@@ -3,9 +3,19 @@
     {{ $store.state.snackbar.text }}
 
     <template v-slot:action="{ attrs }">
-      <v-btn color="pink" text v-bind="attrs" @click="$store.state.snackbar.show = false">
+      <v-btn
+        color="pink"
+        text
+        v-bind="attrs"
+        @click="$store.state.snackbar.show = false"
+      >
         Close
       </v-btn>
     </template>
   </v-snackbar>
 </template>
+
+<style lang="sass">
+div.v-snack:not(.v-snack--absolute)
+  height: 100%
+</style>
